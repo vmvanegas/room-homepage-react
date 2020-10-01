fc5c9704b3c4eb05d09c4561c3db2a8cdbca7ac1
vmvanegas <58665973+vmvanegas@users.noreply.github.com>
2020-10-01 14:38:28 -0500

    Updates

---
+++ precache-manifest.01728591ae7883c6c92942ed7d8b9467.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbabf199a569916c0330ca535214e63",
+    "url": "/room-homepage-react/index.html"
+  },
+  {
+    "revision": "e1a29bd9d6978f9c2026",
+    "url": "/room-homepage-react/static/css/main.9fc77033.chunk.css"
+  },
+  {
+    "revision": "6854232672544e6219d3",
+    "url": "/room-homepage-react/static/js/2.49ff4157.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/room-homepage-react/static/js/2.49ff4157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a29bd9d6978f9c2026",
+    "url": "/room-homepage-react/static/js/main.d5a37f06.chunk.js"
+  },
+  {
+    "revision": "26a54b76b0b56b5d9754",
+    "url": "/room-homepage-react/static/js/runtime-main.2a31ad9c.js"
+  },
+  {
+    "revision": "013946438a56b8adb0f8a258744097b6",
+    "url": "/room-homepage-react/static/media/desktop-image-hero-1.01394643.jpg"
+  },
+  {
+    "revision": "03660ebe1a314460126a9cb74b13aae1",
+    "url": "/room-homepage-react/static/media/desktop-image-hero-2.03660ebe.jpg"
+  },
+  {
+    "revision": "63998c1c2f52977e90efcabe0da730ed",
+    "url": "/room-homepage-react/static/media/desktop-image-hero-3.63998c1c.jpg"
+  },
+  {
+    "revision": "09c8dfd5db5a83a529a50737eeae21f9",
+    "url": "/room-homepage-react/static/media/icon-angle-left.09c8dfd5.svg"
+  },
+  {
+    "revision": "2f95dbcbb0d41f53d9e6d78ecfdd2d9a",
+    "url": "/room-homepage-react/static/media/icon-angle-right.2f95dbcb.svg"
+  },
+  {
+    "revision": "c011ad3b576520b80a56f055ea3ca9b5",
+    "url": "/room-homepage-react/static/media/icon-arrow.c011ad3b.svg"
+  },
+  {
+    "revision": "2992ccac683d5dac7600a04d23abf702",
+    "url": "/room-homepage-react/static/media/icon-close.2992ccac.svg"
+  },
+  {
+    "revision": "0b39db49029721522332d26ba10b2737",
+    "url": "/room-homepage-react/static/media/icon-hamburger.0b39db49.svg"
+  },
+  {
+    "revision": "fa292a671bd24f028df716cb0b106381",
+    "url": "/room-homepage-react/static/media/image-about-dark.fa292a67.jpg"
+  },
+  {
+    "revision": "11c1e8e670af79117d6ade5868882f59",
+    "url": "/room-homepage-react/static/media/logo.11c1e8e6.svg"
+  },
+  {
+    "revision": "6ee5d72deab99ccf2c8fb4f9af9237ba",
+    "url": "/room-homepage-react/static/media/mobile-image-hero-1.6ee5d72d.jpg"
+  }
+]);
